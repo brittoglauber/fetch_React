58bce1551eebe32439a3c60f04e45d3e4a9c028c
brittoglauber <brittoglauber@gmail.com>
2022-06-16 14:40:00 -0300

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Axios from './components/Axios';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Axios />
+  </React.StrictMode>
+);
